d7c0d63a36b0d9445c9a8bdf77431702584d1147
abhinay chaudhary <abhinaychaudhary07@gmail.com>
2026-06-03 17:19:35 +0530

    Fix: use HuggingFace cloud API for embeddings to solve Render OOM error

--- app/services/vector_store.py
+++ app/services/vector_store.py
@@ -1,22 +1,18 @@
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from app.config import load_dotenv
+import os
+
+load_dotenv()
+
 
 class VectorStoreService:
 
     def __init__(self):
-
-        self.embeddings = (
-            HuggingFaceEmbeddings(
-                model_name=
-                "sentence-transformers/all-MiniLM-L6-v2"
-            )
+        self.embeddings = HuggingFaceEndpointEmbeddings(
+            model="sentence-transformers/all-MiniLM-L6-v2",
+            huggingfacehub_api_token=os.getenv("HF_TOKEN")
         )
 
     def create_store(self, docs):
-
-        store = FAISS.from_documents(
-            docs,
-            self.embeddings
-        )
-
-        return store
+        return FAISS.from_documents(docs, self.embeddings)
